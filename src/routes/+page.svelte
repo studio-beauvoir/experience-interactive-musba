ab0276743d70159eb2758677e62dadc10f462525
Arthaud <arthaudp33@gmail.com>
2023-01-31 14:49:36 +0100

    feat: add tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="text-pink-500 font-bold text-3xl">This is a tailwind test</div>
