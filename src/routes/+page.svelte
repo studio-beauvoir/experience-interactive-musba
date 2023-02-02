791d8ddc9484e6524ffa831c4c5e19c94c160e79
Arthaud <arthaudp33@gmail.com>
2023-02-02 10:47:30 +0100

    fix: game title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,6 @@
 </script>
 
 <div class="flex flex-col items-center justify-center h-full">
-    <h1 class="text-pink-500 font-bold text-3xl">This is a tailwind test</h1>
+    <h1 class="text-pink-500 font-bold text-3xl">Too well stolen</h1>
     <Button handleClick={startGame}>Commencer</Button>
 </div>
