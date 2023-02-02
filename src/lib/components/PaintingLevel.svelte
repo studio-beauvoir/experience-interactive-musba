60e5b33f9241d4ea5f724a8f13db61d08def6d0e
Arthaud <arthaudp33@gmail.com>
2023-02-02 14:53:46 +0100

    feat: inspect suspect

--- src/lib/components/PaintingLevel.svelte
+++ src/lib/components/PaintingLevel.svelte
@@ -4,26 +4,35 @@
 
     export let painting;
 
+    let inspectingSuspect = null;
+
     const dispatch = createEventDispatcher();
 
-    function addSuspect(suspect) {
+    function dispatchSuspectSelected(suspect) {
         dispatch('suspect-selected', {
             suspect
         });
     }
+
+    function inspectSuspect(suspect: string) {
+        inspectingSuspect = suspect
+    }
 </script>
 
 <section class="bg-red">
-    <section class="w-full aspect-square relative overflow-auto">
-        <img alt="{painting.name}" class="mx-auto h-full w-auto max-w-none" src="{painting.image}">
-    </section>
-    <article>
-        <h2 class="text-2xl">Qui est le suspect?</h2>
-    </article>
-    <section class="flex flex-col">
-        {#each painting.suspects as suspect}
-            <Button handleClick={()=>addSuspect(suspect)}>{ suspect }</Button>
-        {/each}
-    </section>
-
+    {#if inspectingSuspect}
+        <div>suspect {inspectingSuspect}</div>
+    {:else}
+        <section class="w-full aspect-square relative overflow-auto">
+            <img alt="{painting.name}" class="mx-auto h-full w-auto max-w-none" src="{painting.image}">
+        </section>
+        <article>
+            <h2 class="text-2xl">Qui est le suspect?</h2>
+        </article>
+        <section class="flex flex-col">
+            {#each painting.suspects as suspect}
+                <Button handleClick={()=>inspectSuspect(suspect)}>{ suspect }</Button>
+            {/each}
+        </section>
+    {/if}
 </section>
