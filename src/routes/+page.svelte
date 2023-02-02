7983ec70142c127a82373b3b0f7edef306220302
Arthaud <arthaudp33@gmail.com>
2023-02-02 10:37:09 +0100

    feat: start button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,13 @@
 <script>
     import Button from "../lib/components/Button.svelte";
+    import {goto} from "$app/navigation";
+
+    function startGame() {
+        goto('/game')
+    }
 </script>
 
 <div class="flex flex-col items-center justify-center h-full">
     <h1 class="text-pink-500 font-bold text-3xl">This is a tailwind test</h1>
-    <Button handleClick={()=>alert('coucou')}>Commencer</Button>
+    <Button handleClick={startGame}>Commencer</Button>
 </div>
