97f4d21b3132493f4063fe2bb799b255d6918a21
Arthaud <arthaudp33@gmail.com>
2023-02-02 10:31:34 +0100

    feat: add suspect selection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,6 @@
-<script>
-    import Button from "../lib/components/Button.svelte";
-</script>
+<script></script>
 
 <div class="flex flex-col items-center justify-center h-full">
     <h1 class="text-pink-500 font-bold text-3xl">This is a tailwind test</h1>
-    <Button handleClick={()=>alert('coucou')}>Commencer</Button>
+    <a href="/game">Commencer</a>
 </div>
---
+++ src/routes/game/+page.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import {selectedSuspects} from "$lib/stores/selectedSuspects";
+    import SuspectsSelection from "$lib/components/screens/SuspectsSelection.svelte";
+
+    let painting = 0;
+
+    const paintingsSuspects = [
+        [
+            'bab',
+            'bib',
+            'bob',
+        ],
+        [
+            'charles',
+            'chirles',
+            'chorles',
+        ],
+        [
+            'darling',
+            'dirling',
+            'dorling',
+        ]
+    ];
+
+    function handleSuspectSelection(event) {
+        selectedSuspects.update(function (s) {
+            s.push(event.detail.suspect)
+            return s;
+        });
+
+        painting++;
+    }
+</script>
+
+<section>
+    <div>{ painting }</div>
+    <div>{ $selectedSuspects}</div>
+    {#if painting < paintingsSuspects.length}
+        <SuspectsSelection on:suspect-selected={handleSuspectSelection} suspectsList={paintingsSuspects[painting]}/>
+    {/if}
+</section>
