a2d4179f955ee6895d1710be3c312b5a9e77b453
Maxime <lust.maxime@outlook.fr>
2023-02-07 14:47:50 +0100

    feat: add svg infos

--- src/routes/game/infos/+page.svelte
+++ src/routes/game/infos/+page.svelte
@@ -8,8 +8,9 @@
 </script>
 
 <section class="flex flex-col items-center justify-center h-screen bg-black overflow-hidden px-14">
-    <h1 class="text-yellow text-h2">Bon à</h1>
-    <h1 class="text-yellow text-h1">Savoir</h1>
-    <p class="text-label text-yellow text-center my-16">Pour profiter pleinement de l’expérience, l’idéal est de se trouver dans la salle “Les années 1910-1940 : du cubisme au retour à l’ordre”. <br><br> N’hésitez pas à y explorer les différentes œuvres et à rester attentifs à leurs personnages. Leurs répliques ne seront pas vos seuls indices...</p>
+    <img class="absolute z-0" src="/assets/triangle-svg.svg" alt="">
+    <h1 class="text-yellow text-h2 z-10">Bon à</h1>
+    <h1 class="text-yellow text-display z-10">Savoir</h1>
+    <p class="text-label text-yellow text-center my-16 z-10">Pour profiter pleinement de l’expérience, l’idéal est de se trouver dans la salle “Les années 1910-1940 : du cubisme au retour à l’ordre”. <br><br> N’hésitez pas à y explorer les différentes œuvres et à rester attentifs à leurs personnages. Leurs répliques ne seront pas vos seuls indices...</p>
     <Button handleClick={startIntro}>Continuer</Button>
 </section>
