9436dc4c8e9d5f9923f36ff4504f8d3858c49851
Arthaud <arthaudp33@gmail.com>
2023-02-09 16:43:51 +0100

    feat: wording info page

--- src/routes/game/infos/+page.svelte
+++ src/routes/game/infos/+page.svelte
@@ -7,13 +7,18 @@
     }
 </script>
 
-<img alt="" class="absolute w-full z-0" src="/assets/triangle-svg.svg">
-<section class="flex flex-col items-center justify-center h-full bg-black overflow-hidden px-14">
-    <h1 class="text-yellow text-h2 z-10">Bon à</h1>
-    <h1 class="text-yellow text-display z-10">Savoir</h1>
-    <p class="text-p text-yellow text-center my-16 z-10">Pour profiter pleinement de l’expérience, l’idéal est de se
-        trouver dans la salle “Les années 1910-1940 : du cubisme au retour à l’ordre”. <br><br> N’hésitez pas à y
-        explorer les différentes œuvres et à rester attentifs à leurs personnages. Leurs répliques ne seront pas vos
-        seuls indices...</p>
-    <PrimaryButton handleClick={startIntro}>C'est compris !</PrimaryButton>
+<img alt=" " class="absolute w-full" src="/assets/triangle-svg.svg">
+<section class="relative flex flex-col items-center justify-center h-full overflow-hidden px-14">
+    <p class="text-yellow text-h2">Votre</p>
+    <h1 class="text-yellow text-display">Mission</h1>
+    <p class="text-p text-yellow text-center pt-16 pb-4">
+        Pour profiter pleinement de l’expérience, l’idéal est de se trouver dans la salle “Les années 1910-1940 : du
+        cubisme au retour à l’ordre”.
+    </p>
+    <p class="text-p text-yellow text-center pb-16">
+        N’hésitez pas à y explorer les différentes œuvres et à rester attentifs à leurs personnages. Leurs répliques
+        ne
+        seront pas vos seuls indices...
+    </p>
+    <PrimaryButton handleClick={startIntro}>Commencer l'enquête</PrimaryButton>
 </section>
