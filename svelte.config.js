0f650a770c5bf6dbd774b11799b45c7001d8c1b1
Arthaud <arthaudp33@gmail.com>
2023-02-09 14:20:45 +0100

    feat: add correct aliases

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
     kit: {
         adapter: adapter(),
         alias: {
-            $db: './src/db'
+            $db: './src/db',
         }
     }
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
+import {sveltekit} from '@sveltejs/kit/vite';
+import type {UserConfig} from 'vite';
+import path from "path";
 
 const config: UserConfig = {
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+    plugins: [sveltekit()],
+    test: {
+        include: ['src/**/*.{test,spec}.{js,ts}']
+    },
+    resolve: {
+        alias: {
+            '$lib': path.resolve('./src/lib/'),
+            '$db': path.resolve('./src/db/'),
+        }
+    }
 };
 
 export default config;
