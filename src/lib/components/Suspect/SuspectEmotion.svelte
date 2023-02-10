e7ad87b919794556d29ba34128d3f4b715870b10
Arthaud <arthaudp33@gmail.com>
2023-02-10 16:40:23 +0100

    feat: display gif

--- src/lib/components/Suspect/SuspectEmotion.svelte
+++ src/lib/components/Suspect/SuspectEmotion.svelte
@@ -6,18 +6,22 @@
     export let handleClick;
 
     const widthInPercent = 28;
+
+    $: size = document.body.offsetWidth*widthInPercent/100;
+    $: gifStyle = `height: ${size}px; width: ${size}px`;
 </script>
 
 <button on:click={handleClick} class="absolute translate-y-1/2 -translate-x-1/2" style="left: {position.x}%; bottom: {position.y}%;">
-    <LottiePlayer
-            src="/lotties/{suspect.id}.json"
-            autoplay="{true}"
-            loop="{true}"
-            controls="{false}"
-            renderer="svg"
-            controlsLayout="{[]}"
-            background="transparent"
-            height="{document.body.offsetWidth*widthInPercent/100}"
-            width="{document.body.offsetWidth*widthInPercent/100}"
-    />
+    <img src="/emotions/{suspect.id}.gif" class="max-w-none" style={gifStyle}/>
+<!--    <LottiePlayer-->
+<!--            src="/lotties/{suspect.id}.json"-->
+<!--            autoplay="{true}"-->
+<!--            loop="{true}"-->
+<!--            controls="{false}"-->
+<!--            renderer="svg"-->
+<!--            controlsLayout="{[]}"-->
+<!--            background="transparent"-->
+<!--            height="{document.body.offsetWidth*widthInPercent/100}"-->
+<!--            width="{document.body.offsetWidth*widthInPercent/100}"-->
+<!--    />-->
 </button>
