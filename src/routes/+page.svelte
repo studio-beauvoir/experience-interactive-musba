856ed7479faf86b31e7d56f9b6fa5256d5294dfb
Maxime <lust.maxime@outlook.fr>
2023-02-10 12:38:57 +0100

    fix: font h1 & title home and infos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,7 +16,7 @@
 
 <div class="flex flex-col items-center justify-center h-full bg-black">
     <img alt=" " class="absolute h-full w-full z-0" src="/assets/triangle-svg.svg">
-    <h1 class="text-yellow text-h2 z-10">Too well</h1>
+    <h1 class="text-yellow text-h1 pr-32 z-10">Too well</h1>
     <h1 class="text-yellow text-display z-10">Stolen</h1>
     <div class="z-10 m-8 px-10 py-4">
         <img alt="" class="h-auto" src="/images/home-page-image.webp">
