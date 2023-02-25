2ec9886e53d774ba25292842da5d3dce777c4b90
Arthaud <arthaudp33@gmail.com>
2023-02-25 23:19:09 +0100

    feat: scrolling comic strip

--- src/lib/components/Result/Tabs/PathTab.svelte
+++ src/lib/components/Result/Tabs/PathTab.svelte
@@ -4,15 +4,19 @@
     import FeedBack from "$lib/components/Result/FeedBack.svelte";
 </script>
 
-<section class="flex flex-col gap-12 px-6">
+<section class="flex flex-col gap-12 px-6 w-full">
     <img alt="" class="absolute -z-10 top-30 right-0" src="/assets/triangle-solo-1.svg">
 
     <FeedBack suspects={$selectedSuspects}/>
 
     <div class="bg-brown h-px -mx-6"></div>
 
-    <div class="flex flex-col gap-10">
-        <h1 class="text-h1 text-yellow mx-auto">Votre BD</h1>
-        <ComicsStrip suspects={$selectedSuspects}/>
+    <div class="flex flex-col relative text-center">
+        <h1 class="text-h1 text-yellow mb-1">Votre BD</h1>
+        <span class="mb-8">(scrollez vers la droite)</span>
+
+        <section class="-mx-6">
+            <ComicsStrip suspects={$selectedSuspects}/>
+        </section>
     </div>
 </section>
