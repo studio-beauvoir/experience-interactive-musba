c9e7329eb8388724bd500102e5bac4a343242d4e
Arthaud <arthaudp33@gmail.com>
2023-03-19 23:55:29 +0100

    feat: walking hint

--- src/lib/components/Painting/PaintingIntroduction.svelte
+++ src/lib/components/Painting/PaintingIntroduction.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import {fade} from 'svelte/transition';
+    import {LottiePlayer} from '@lottiefiles/svelte-lottie-player';
 
     export let painting;
     export let transitionDuration;
@@ -7,25 +8,42 @@
 
 </script>
 
-<button class="absolute inset-0 bg-semi-transparent flex flex-col gap-2 justify-center items-center text-white"
+<button class="absolute inset-0 bg-semi-transparent gap-2 items-center text-white"
         on:click={handleClick} transition:fade={{ duration: transitionDuration }}>
-    <section class="w-full flex gap-6 items-center">
-        <section class="flex flex-col gap-2 w-full">
-            <div class="bg-white h-px"></div>
-            <div class="bg-white h-px mr-3"></div>
-            <div class="bg-white h-px"></div>
+    <section class="absolute top-5/10 w-full -translate-y-1/2">
+        <section class="w-full flex gap-6 items-center">
+            <section class="flex flex-col gap-2 w-full">
+                <div class="bg-white h-px"></div>
+                <div class="bg-white h-px mr-3"></div>
+                <div class="bg-white h-px"></div>
+            </section>
+
+            <section class="text-center">
+                <h1 class="text-h1 italic">{painting.name}</h1>
+            </section>
+
+            <section class="flex flex-col gap-2 w-full">
+                <div class="bg-white h-px"></div>
+                <div class="bg-white h-px ml-3"></div>
+                <div class="bg-white h-px"></div>
+            </section>
         </section>
 
-        <section class="text-center">
-            <h1 class="text-h1 italic">{painting.name}</h1>
-        </section>
-
-        <section class="flex flex-col gap-2 w-full">
-            <div class="bg-white h-px"></div>
-            <div class="bg-white h-px ml-3"></div>
-            <div class="bg-white h-px"></div>
-        </section>
+        <p class="text-p mt-2">{painting.author}, {painting.date}</p>
     </section>
 
-    <p class="text-p">{painting.author}, {painting.date}</p>
+    <section class="absolute top-3/10 w-full -translate-y-1/2 flex flex-col">
+        <div class="relative h-24">
+            <LottiePlayer
+                    autoplay="{true}"
+                    background="transparent"
+                    loop="{true}"
+                    renderer="svg"
+                    src="/lotties/walk.json"
+                    style="height: 100%; width: auto; aspect-ratio: 1 / 1; margin: auto"
+            />
+        </div>
+
+        <p class="text-yellow italic">Marchez jusqu'au tableau</p>
+    </section>
 </button>
