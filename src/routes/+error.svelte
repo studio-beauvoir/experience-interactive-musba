cd8bab195d928028b380491893ee161a5ecba9c8
Arthaud <arthaudp33@gmail.com>
2023-03-21 20:41:24 +0100

    feat: add test dev page
    qsdqs

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,7 +1,7 @@
 <script>
     import {fade} from 'svelte/transition';
 </script>
-<div class="flex flex-col items-center justify-center h-full" transition:fade>
-    <h1 class="text-center">There was an error loading this page</h1>
-    <a class="underline" href="/">Back to home</a>
+<div class="flex flex-col items-center justify-center h-full text-white" transition:fade>
+    <h1 class="text-center">Une erreur est survenue</h1>
+    <a class="underline" href="/">Retour à l'accueil</a>
 </div>
